bda13dfde256023ddaf070e51b34ae709a9c49ab
finanovita <finanovita479@gmail.com>
2025-11-28 10:01:43 +0700

    Inisialisasi database Room DatabaseSiswa dan akses SiswaDao

---
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+
+}
