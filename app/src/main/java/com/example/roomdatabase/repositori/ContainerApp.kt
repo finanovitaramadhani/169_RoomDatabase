6e6c5c5667bb24ea6cc3b790c359a42f418605a2
finanovita <finanovita479@gmail.com>
2025-11-28 10:20:01 +0700

    Implementasi ContainerDataApp untuk inject repository lokal Siswa

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.roomdatabase.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.roomdatabase.room.DatabaseSiswa
 
@@ -7,3 +8,13 @@ interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+
+}
