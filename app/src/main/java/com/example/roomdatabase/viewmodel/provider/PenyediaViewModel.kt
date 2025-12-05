2f6c0260f7c741387377414ee93a638562b4c22b
finanovita <finanovita479@gmail.com>
2025-12-05 13:35:37 +0700

    menambahkan initializer untuk DetailViewModel dan EditViewModel

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -2,10 +2,13 @@ package com.example.roomdatabase.viewmodel.provider
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
+import com.example.roomdatabase.viewmodel.EditViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
@@ -13,6 +16,21 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        //edit : tambah initializer untuk DetailViewModel dan EditViewModel
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiSiswa().container.repositoriSiswa
             )
         }
